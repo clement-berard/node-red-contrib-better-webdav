261fa209f4e50f5dc1a23bf2b3d4a5f0ada87af6
Clément BERARD <contact@clementberard.com>
2024-11-23 18:31:28 +0100

    up

--- src/nodes/webdav-config/editor/index.ts
+++ src/nodes/webdav-config/editor/index.ts
@@ -1,11 +1,9 @@
 import { type NodeEditorProps, createEditorNode } from '@keload/node-red-dxp/editor';
-import { watchInput } from '@keload/node-red-dxp/editor/dom-helper';
 import { validators } from '@keload/node-red-dxp/utils';
 import type { NodeCredentials, NodeProps } from '../types';
 
 const WebdavConfig = createEditorNode<NodeEditorProps<NodeProps>, NodeCredentials>({
   category: 'config',
-  color: '#a6bbcf',
   defaults: {
     name: { value: '', required: true },
     basePath: { value: '' },
@@ -15,17 +13,8 @@ const WebdavConfig = createEditorNode<NodeEditorProps<NodeProps>, NodeCredential
     user: { type: 'text' },
     password: { type: 'password' },
   },
-  inputs: 1,
-  outputs: 1,
-  icon: 'font-awesome/fa-tower-broadcast',
   label: function () {
     return this.name || 'WebdavConfig';
-  },
-  oneditprepare: () => {
-    // $age == #node-input-age in DOM
-    watchInput('$age', ([ageValue]) => {
-      console.log('ageValue', ageValue);
-    });
   },
 });
 
